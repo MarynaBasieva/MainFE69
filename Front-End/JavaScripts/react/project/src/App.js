ff0096238dfca215482b4fb804e89db8d5fde810
Basieva Maryna <verna.job@gmail.com>
2022-12-29 20:39:48 +0200

    cw47

---
+++ Front-End/JavaScripts/react/project/src/App.js
@@ -0,0 +1,85 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Nav />
+      <Baner />
+      <Blog />
+      <Footer />
+    </div>
+  );
+}
+
+const Header =() =>{
+  return(
+   <header className="header">
+     <div className="container">
+        <div>
+          <a href="/">
+            <img src="logo.svg" alt="logo" />
+          </a>
+        </div>
+     </div>
+   </header>
+  )
+}
+const Nav =() =>{
+  return(
+      <nav className="nav">
+        <div className="container">
+          <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+  )
+}
+
+const Baner =() =>{
+  return(
+      <section className="baner">
+        <div className="container">
+          <div className="box_baner">
+            <h1>Heading</h1>
+             <p>LOrem ipsum dolar LOrem ipsum dolar LOrem ipsum dolar LOrem ipsum dolar LOrem ipsum dolar LOrem ipsum dolar LOrem ipsum dolar</p>
+          </div>
+        </div>
+      </section>
+  )
+}
+
+const Blog =() =>{
+  return(
+      <section className="blog">
+        <div className="container">
+          <div className="blog_post">
+            <div>post 1</div>
+            <div>post 2</div>
+            <div>post 3</div>
+          </div>
+        </div>
+      </section>
+  )
+}
+
+const Footer =() =>{
+  return(
+      <footer className="footer">
+        <div className="container">
+          <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Contact</a></li>
+          </ul>
+        </div>
+      </footer>
+  )
+}
+
+export default App;
